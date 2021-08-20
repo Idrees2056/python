2317b97c3bab969d29648b1b0b0e41262c346c7e
Idrees2056 <73420329+Idrees2056@users.noreply.github.com>
2021-08-20 12:09:41 +0530

    Add files via upload

---
+++ if else.py
@@ -0,0 +1,8 @@
+x=12
+y= int( input("enter your guess number"))
+if y>x:
+    print("you have guessed a greater number")
+elif y==x:
+    print("your guess is correct")
+else :
+    print("you have guessed a smaller number") 
